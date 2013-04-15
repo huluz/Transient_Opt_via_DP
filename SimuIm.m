14948040d9412681286cf3067698be55c1bbe4a4
ZhangXiaorui <zxri8700@gmail.com>
2013-04-15 16:58:51 +0800

    计算文件内构造函数失败
    由于构造出的每个子函数都是一个Function
    Handle，由这些Handle构成的Cell无法传递给Fsolve函数。可以尝试使用长字符串来构建所需函数。后续通过手动输入的办法先完成模拟计算后再考虑这个问题。

--- SimuIm.m
+++ SimuIm.m
@@ -1,6 +1,8 @@
 %采用隐式算法进行非稳态模拟
 
 clc;clear;
+
+%气体参数
 mol = 17.44;            		%气体分子质量
 R = 8314;               		%通用气体常数
 PreC = 46.59E5;         		%临界压力
@@ -10,56 +12,94 @@ Temp_sta = 20+273.15;   	%标况温度
 Pre_sta = 101325;       		%标况压力
 Den_air = 1.209;        		%空气密度
 Rel_Den = 0.6;          		%天然气相对密度
-Den_sta = Rel_Den*Den_air;  %天然气标况密度
-alpha = Den_sta*Temp_sta/Pre_sta/Temp;  %通用气体方程参数
-beta = 0.257/PreC - 0.533*TempC/PreC/Temp;  %AGA气体状态方程参数
+Den_sta = Rel_Den*Den_air;  	%天然气标况密度
+alpha = Den_sta*Temp_sta/Pre_sta/Temp;  	%通用气体方程参数
+beta = 0.257/PreC - 0.533*TempC/PreC/Temp; 	%AGA气体状态方程参数
 
-Len = 5E3;            		%管段长度
-Time = 24*3600;		%模拟时长
-dt = 3600;              		%时步
-dx = 2E3;               		%稳态模拟空间步长
+%管段及模拟参数
+Len = 100E3;            		%管段长度
 lamda = 0.008;          		%摩阻系数
 Din = 0.6096;           		%管段内径
+C0 = 0.03848;           		%稳态模拟公式系数
+Time = 24*3600;		%模拟时长
+Time_Sec = 3600;		%单个时间段时长
+dt = 60;              		%时步
+TimeSteps_Total = Time / dt; 	%总时步数
+TimeSteps_Per_Sec = Time_Sec / dt;		%每个时间段的步长数
+dx = 10E3;               		%空间步长
+SpaceSteps = Len / dx;		%空间分段数
 
-TimeSteps = Time/dt; 		%时步数
-Ps = 7.3E6*ones(24,1);		%管段起点压力
-Qs = zeros(24,1);		%起点压力
-Mss = zeros(24,1);		%起点流量
-Pe = zeros(24,1);		%终点压力
+%边界条件
+Ps = 7.3E6*ones(TimeSteps_Total,1);		%管段起点压力
+Qs = zeros(TimeSteps_Total,1);		%起点流量
+Mss = zeros(TimeSteps_Total,1);		%起点质量流量密度
+Pe = zeros(TimeSteps_Total,1);		%终点压力
 Qbasic = 65;           		 %流量基数
 Ff = [0.2;0.15;0.1;0.25;0.35;0.58;1.2;1.3;1;0.97;0.85;1.65;2;1;0.8;0.65;...
-    1.15;1.9;2.8;2.2;1.2;0.85;0.5;0.35];    %小时流量不均匀系数
-Qe = Qbasic*Ff;         		%终点流量
-Mse = Den_sta*Qe;		%终点质量流量密度
-
-C0 = 0.03848;           		%稳态模拟公式系数
+    1.15;1.9;2.8;2.2;1.2;0.85;0.5;0.35];    		%小时流量不均匀系数
+Qe = zeros(TimeSteps_Total,1);         		%终点流量
+for i = 1:Time/Time_Sec 			%根据时间点上的值设定整个时间段的流量
+	Qe(TimeSteps_Per_Sec*(i-1)+1:TimeSteps_Per_Sec*i) = Qbasic*Ff(i)*ones(TimeSteps_Per_Sec,1);
+end
+Mse = Den_sta * Qe;		%终点质量流量密度
 
 %稳态模拟
 tl = Len;               		%管段长度
 Pls = Ps(1);            		%起点压力
+i = 1;
+Pressure(i) = Pls;		%沿线压力记录
 while tl>0              		%稳态模拟
     z = 1 + beta*Pls;   		%压缩因子
-    Ple = Pls^2 - lamda*z*Rel_Den*Temp*dx*Qe(24)^2/C0^2/Din^5;
+    Ple = Pls^2 - lamda*z*Rel_Den*Temp*dx*Qe(TimeSteps_Total)^2/C0^2/Din^5;
     Ple = Ple^0.5;
+    i = i+1;
+    Pressure(i) = Ple;
     tl = tl - dx;
     Pls = Ple;
 end
-Ps_ini = Ps(1);            		%起点初始压力
-Mss_ini = Den_sta*Qe(24);	%初始质量流量密度
-Pe_ini = Ple;       		%终点质量流量密度
-Mse_ini = Den_sta*Qe(24);  	%初始质量流量密度
+MassFlux = Den_sta*Qe(TimeSteps_Total)*ones(SpaceSteps+1,1);	%构造初始条件-质量流量密度
+Pressure = Pressure';		%压力
+figure(1);
+plot(Pressure);
+title('Pressure');
+figure(1);
+plot(MassFlux);
+title('MassFlux');
 
 %瞬态模拟
+
 %第一时步
-tf = @(x)transfun(x,dt,Len,alpha,beta,lamda,Din,Ps_ini,Pe_ini,Mss_ini,Mse_ini,Ps(1),Mse(1));	%构造方程
-x0 = [Pe_ini,Mss_ini];		%准备初值
+%构造管流控制方程组
+tf{1} = @(x)((alpha*Ps(1)/(1+beta*Ps(1))+alpha*x(2)/(1+beta*x(2))-alpha*Pressure(1)/(1+beta*Pressure(1))-alpha*Pressure(2)/(1+beta*Pressure(2)))/2/dt + (Mse(1)+MassFlux(2)-x(1)-MassFlux(1))/2/dx);
+tf{2} = @(x)((x(1)+x(3)-MassFlux(1)-MassFlux(2))/2/dt +((x(3)^2)*(1+beta*x(2))/alpha/x(2)+x(2)+(MassFlux(2)^2)*(1+beta*Pressure(2))/alpha/Pressure(2)+Pressure(2)-(x(1)^2)*(1+beta*Ps(1))/alpha/Ps(1)-Ps(1)-(MassFlux(1)^2)*(1+beta*Pressure(1))/alpha/Pressure(1)-Pressure(1))/2/dx + lamda*((x(1)^2)*(1+beta*Ps(1))/alpha/Ps(1)+(x(3)^2)*(1+beta*x(2))/alpha/x(2)+(MassFlux(1)^2)*(1+beta*Pressure(1))/alpha/Pressure(1)+(MassFlux(2)^2)*(1+beta*Pressure(2))/alpha/Pressure(2))/8/Din);
+for i = 2:SpaceSteps-1 
+	tf{2*i-1} = @(x)((alpha*x(2*i)/(1+beta*x(2*i-2))+alpha*x(2*i)/(1+beta*x(2*i))-alpha*Pressure(i)/(1+beta*Pressure(i))-alpha*Pressure(i+1)/(1+beta*Pressure(i+1)))/2/dt + (x(2*i+1)+MassFlux(i+1)-x(2*i-1)-MassFlux(i))/2/dx);
+	tf{2*i}  = @(x)((x(2*i-1)+x(2*i+1)-MassFlux(i)-MassFlux(i+1))/2/dt + ((x(2*i+1)^2)*(1+beta*x(2*i-2))/alpha/x(2*i-2)+x(2*i)+(MassFlux(i+1)^2)*(1+beta*Pressure(i+1))/alpha/Pressure(i+1)+Pressure(i+1)-(x(2*i-1)^2)*(1+beta*x(2*i-2))/alpha/x(2*i-2)-x(2*i-2)-(MassFlux(i)^2)*(1+beta*Pressure(i))/alpha/Pressure(i)-Pressure(i))/2/dx + lamda*((x(2*i+1)^2)*(1+beta*x(2*i-2))/alpha/x(2*i-2)+(MassFlux(i+1)^2)*(1+beta*Pressure(i+1))/alpha/Pressure(i+1)+(x(2*i-1)^2)*(1+beta*x(2*i-2))/alpha/x(2*i-2)+(MassFlux(i)^2)*(1+beta*Pressure(i))/alpha/Pressure(i))/8/Din);
+end
+tf{2*SpaceSteps-1} = @(x)((alpha*x(2*SpaceSteps-2)/(1+beta*x(2*SpaceSteps-2))+alpha*x(2*SpaceSteps)/(1+beta*x(2*SpaceSteps))-alpha*Pressure(SpaceSteps)/(1+beta*Pressure(SpaceSteps))-alpha*Pressure(SpaceSteps+1)/(1+Pressure(SpaceSteps+1)))/2/dt + (Mse(1)+MassFlux(SpaceSteps+1)-x(2*SpaceSteps-1)-MassFlux(SpaceSteps))/2/dx);
+tf{2*SpaceSteps} = @(x)(x(2*SpaceSteps-1)+Mse(1)-MassFlux(SpaceSteps)-MassFlux(SpaceSteps+1))/2/dt + ((Mse(1)^2)*(1+beta*x(2*SpaceSteps))/alpha/x(2*SpaceSteps)+x(2*SpaceSteps)+(MassFlux(SpaceSteps+1)^2)*(1+beta*Pressure(SpaceSteps+1))/alpha/Pressure(SpaceSteps+1)+Pressure(SpaceSteps+1)-(x(2*i-1)^2)*(1+beta*x(2*SpaceSteps-2))/alpha/x(2*SpaceSteps-2)-x(2*SpaceSteps-2)-(MassFlux(SpaceSteps)^2)*(1+beta*Pressure(SpaceSteps))/alpha/Pressure(SpaceSteps)-Pressure(SpaceSteps-1))/2/dx + lamda*((Mse(1)^2)*(1+beta*x(2*SpaceSteps))/alpha/x(2*SpaceSteps)+(MassFlux(SpaceSteps+1)^2)*(1+beta*Pressure(SpaceSteps+1))/alpha/Pressure(SpaceSteps+1)+(x(2*i-1)^2)*(1+beta*x(2*SpaceSteps-2))/alpha/x(2*SpaceSteps-2)+(MassFlux(SpaceSteps)^2)*(1+beta*Pressure(SpaceSteps))/alpha/Pressure(SpaceSteps))/8/Din;
+tf = tf';
+%tf = @(x)transfun(x,dt,Len,alpha,beta,lamda,Din,Ps_ini,Pe_ini,Mss_ini,Mse_ini,Ps(1),Mse(1));	%构造方程
+x0 = zeros(2*SpaceSteps);	%准备初值
+x0(1) = MassFlux(1);
+for i = 2:SpaceSteps
+	x0(2*i-2) = Pressure(i);
+	x0(2*i-1) = MassFlux(i);
+end
+x0(2*SpaceSteps) = Pressure(SpaceSteps+1);
 %options = optimset('MaxFunEvals',1E4,'MaxIter',1E4,'Display','iter-detailed');
 results = fsolve(tf,x0);		%计算
-Mss(1) = results(2);		%归档计算结果
-Qs(1) = Mss(1) / Den_sta;
-Pe(1) = results(1);
+MassFlux(1) = results(1);	%归档计算结果
+for i = 2: SpaceSteps
+	Pressure(i) = results(2*i-2);
+	MassFlux(i) = results(2*i-1);
+end
+Pressure(SpaceSteps) = results(2*SpaceSteps);
+Pressure(1) = Ps(2);		%引入边界条件
+MassFlux(SpaceSteps+1) = Mse(2);
 
-for i = 2:TimeSteps
+%后续过程
+for i = 2:2
 	tf = @(x)transfun(x,dt,Len,alpha,beta,lamda,Din,Ps(i-1),Pe(i-1),Mss(i-1),Mse(i-1),Ps(i),Mse(i));	%构造方程
 	x0 = [Pe(i-1),Mss(i-1)];	%准备初值
 	results = fsolve(tf,x0);	%计算
